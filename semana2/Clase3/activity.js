40e912ced13947c615aaada0c1e8ba423303df06
kevin-fulcrum <kevinariassistemas@gmail.com>
2020-10-14 20:29:25 -0500

    actividad

---
+++ semana2/Clase3/activity.js
@@ -0,0 +1,24 @@
+
+
+const div = (a,b) => {
+    console.log('div', a/b)
+    return a/b;
+}
+
+div(20,5);
+
+const cstring = (a) => {
+    b='algo'
+    console.log('cstring', b=b+a)
+    return b=b+a;
+}
+
+cstring(2);
+
+const libreria = (c) =>{
+    let libro = ["libroa","librob"]
+    libro.push(c);
+    console.log(libro);
+}
+
+libreria("libroc");
---
+++ semana2/Clase3/index.js
@@ -0,0 +1,36 @@
+function greet(name){
+    console.log(`Hello!, ${name}!`)
+}
+
+greet('Daniel');
+
+//function add(x,y){
+  //  console.log('add', x + y)
+    //return x +y;
+//}
+
+//add(2,50);
+
+const sum = function add(x,y){
+    console.log('add', x + y)
+    return x +y;
+}
+
+sum(20,5)
+
+const multiply = (x,y) => {
+    console.log('multiply', x * y)
+    return x * y;
+}
+
+multiply(20,5)
+
+const square = x => {
+    console.log('square', x * x)
+    return x * x;
+}
+
+square(5);
+
+const squaresimpify = x => x*x;;
+squaresimpify(7);
